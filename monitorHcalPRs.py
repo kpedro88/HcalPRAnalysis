43baedf73f6441e2f22d820e609aa68139e99a55
Kevin Pedro <kpedro88@gmail.com>
2016-07-20 16:44:51 -0500

    fix bug for updating json entry

--- monitorHcalPRs.py
+++ monitorHcalPRs.py
@@ -2,14 +2,15 @@ import requests
 import json
 
 # list of PRs
-PRs = [12883, 12905, 12916, 12974, 13004, 13139, 13205, 13232, 13255, 13290, 13252, 13331, 13307, 13334, 13389, 13491, 13557, 13567, 13683, 13704, 13832, 13822, 13861, 13868, 13915, 13997, 14101, 14139, 14313, 14314, 14363, 14383, 14456, 14418, 14461, 14482, 14471, 14526, 14594, 14572, 14691, 14749, 14838, 14920, 14599, 14856, 15061, 15092, 15180]
+PRs = [12883, 12905, 12916, 12974, 13004, 13139, 13205, 13232, 13255, 13290, 13252, 13331, 13307, 13334, 13389, 13491, 13557, 13567, 13683, 13704, 13832, 13822, 13861, 13868, 13915, 13997, 14101, 14139, 14313, 14314, 14363, 14383, 14456, 14418, 14461, 14482, 14526, 14594, 14572, 14691, 14749, 14838, 14920, 14599, 14856, 15061, 15092, 15180, 15214]
 
 # open cached database of PR properties
 with open("PRdict.json",'r') as PRfile:
     PRdict = json.load(PRfile)
 
-for PR in PRs:
+for PRnum in PRs:
     # skip finished PRs
+    PR = str(PRnum)
     if PR in PRdict.keys() and (PRdict[PR]["merged"] or PRdict[PR]["closed"]): continue
     # call API (https://developer.github.com/v3/pulls/)
     r = requests.get('https://api.github.com/repos/cms-sw/cmssw/pulls/'+str(PR))
